55a6f9f43ca1bda890c7f8d1187035d591056bdb
Ernicio-Chilundo-Dev <erniciojermiaschilund@gmail.com>
2024-10-29 20:19:05 +0200

    colocado as cores que seram necessarias e plicado as cores

--- ludo.py
+++ ludo.py
@@ -6,3 +6,13 @@ import sys
 pygame.init()
 
 # Cores
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255,0,0)
+BLUE = (0, 0, 255)
+
+
+# Tamanho da tela e do tabulerio
+WIDTH, HEIGHT = 600, 600
+ROWS, COLS = 15, 15 # Ludo e um jogo com 15x15 (neste exemplo)
+SQUARE_SIZE = WIDTH // COLS
